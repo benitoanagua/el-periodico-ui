f59b7b7c6bb42fffc59e89535e9d2f7becb08bc8
Benito Anagua <benito.anagua@gmail.com>
2021-08-15 00:03:04 -0400

    Colors story has been added

--- src/components/Palette/Palette.vue
+++ src/components/Palette/Palette.vue
@@ -1,10 +1,38 @@
 <template>
-  <span>Palette</span>
+  <div :class="paletteClass">
+    <cs-palette-item
+      v-for="item in items"
+      :bgColor="name.toLowerCase()"
+      :range="item.name"
+      :value="item.value"
+    />
+  </div>
 </template>
 
 <script>
+import { reactive, computed } from "vue";
+import CsPaletteItem from "./PaletteItem.vue";
+
 export default {
   name: "CsPalette",
-  setup() {},
+  components: { CsPaletteItem },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      default: null,
+    },
+  },
+  setup(props) {
+    props = reactive(props);
+    return {
+      paletteClass: computed(() => ({
+        "flex flex-col": true,
+      })),
+    };
+  },
 };
 </script>

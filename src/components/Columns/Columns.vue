eb62c5eb47295da1c96a9de031bb92dd7c8fdd64
Benito Anagua <benito.anagua@gmail.com>
2021-08-21 14:17:39 -0400

    Columns has been setted

---
+++ src/components/Columns/Columns.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="columnsClass">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+import CsColumn from "./Column.vue";
+export default {
+  name: "CsColumns",
+  components: { CsColumn },
+  props: {
+    space: {
+      type: String,
+      default: null,
+    },
+    margin: {
+      type: String,
+      default: null,
+    },
+    padding: {
+      type: String,
+      default: null,
+    },
+  },
+  setup(props) {
+    props = reactive(props);
+    return {
+      columnsClass: computed(() => ({
+        flex: true,
+        [`${props.space}`]: true,
+        [`${props.margin}`]: true,
+        [`${props.padding}`]: true,
+      })),
+    };
+  },
+};
+</script>

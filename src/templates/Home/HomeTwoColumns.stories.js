e692eca8dbf7632805a56947842b6d4f3345ffc4
Benito Anagua <benito.anagua@gmail.com>
2021-10-04 10:47:16 -0400

    Home 2 & 3 has added

--- src/templates/Home/HomeTwoColumns.stories.js
+++ src/templates/Home/HomeTwoColumns.stories.js
@@ -5,7 +5,7 @@ import CsContainer from "@/components/Container/Container.vue";
 import CsCard from "@/components/Card/Card.vue";
 
 export default {
-  title: "Design System/Templates/Portada/Home1",
+  title: "Design System/Templates/Home/HomeTwoColumns",
   component: {
     CsSection,
     CsContainer,
@@ -24,9 +24,9 @@ const Template = (args) => ({
     const level2 = ref(null);
     const level3 = ref(null);
     const postsService = ref(new PostsService());
-    postsService.value.get(5).then((data) => (level1.value = data));
-    postsService.value.getPosts(6, 9).then((data) => (level2.value = data));
-    postsService.value.getPosts(10, 15).then((data) => (level3.value = data));
+    postsService.value.get(20).then((data) => (level1.value = data));
+    postsService.value.getPosts(17, 19).then((data) => (level2.value = data));
+    postsService.value.getPosts(1, 6).then((data) => (level3.value = data));
     return { ...args, level1, level2, level3 };
   },
   template: `
@@ -35,46 +35,44 @@ const Template = (args) => ({
       breakpoint="lg"
       maxWidth="max-w-screen-lg"
     >
-      <div class="grid grid-cols-1 ">
-        <div v-for="post in level1">
-          <cs-card
-          :breakpoint="breakpoint"
-          :heading="2"
-          media="right"
-          mediaFraction="w-4/7"
-          bodyFraction="w-3/7"
-          aspectRatio="aspect-w-3 aspect-h-2"
-          url="#"
-          :title="post.title"
-          :image="post.image"
-          :excerpt="post.excerpt"
-          :author="post.author"
-          :category="post.category"
-          :date="post.date"
-          />
+      <div class="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6">
+        <div class="w-full md:w-4/7">
+          <div class="grid grid-cols-1 ">
+            <cs-card
+              v-for="post in level1"
+              :breakpoint="breakpoint"
+              :heading="2"
+              media="top"
+              mediaFraction="w-4/7"
+              bodyFraction="w-3/7"
+              aspectRatio="aspect-w-3 aspect-h-2"
+              url="#"
+              :isFloat="true"
+              :title="post.title"
+              :image="post.image"
+              :excerpt="post.excerpt"
+              :author="post.author"
+              :category="post.category"
+              :date="post.date"
+            />
+          </div>
         </div>
-      </div>
-    </cs-container>
-  </cs-section>
-
-  <cs-section margin="my-6" backgroundColor="bg-white">
-    <cs-container
-      breakpoint="lg"
-      maxWidth="max-w-screen-lg"
-    >
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <cs-card
-          v-for="post in level2"
-          :breakpoint="breakpoint"
-          :heading="5"
-          media="top"
-          aspectRatio="aspect-w-3 aspect-h-2"
-          url="#"
-          :title="post.title"
-          :image="post.image"
-          :category="post.category"
-          :date="post.date"
-        />
+        <div class="w-full md:w-3/7">
+          <div class="grid grid-cols-1 gap-6">
+            <cs-card
+              v-for="post in level2"
+              :breakpoint="breakpoint"
+              :heading="5"
+              media="left"
+              aspectRatio="aspect-w-3 aspect-h-2"
+              url="#"
+              :title="post.title"
+              :image="post.image"
+              :category="post.category"
+              :date="post.date"
+            />
+          </div>
+        </div>
       </div>
     </cs-container>
   </cs-section>
